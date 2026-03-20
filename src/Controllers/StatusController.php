e44591fc7974012256aa88d778b7c6fa2cdfad2c
André Moura <andre.benedicto@etec.sp.gov.br>
2026-03-20 19:22:04 -0300

    Finalizar o endpoint /status

--- src/Controllers/StatusController.php
+++ src/Controllers/StatusController.php
@@ -3,14 +3,32 @@
 namespace CardCollection\Controllers;
 
 use CardCollection\Infra\Database;
+use DateTime;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
 class StatusController
 {
     public function showStatus(): JsonResponse
     {
+        $updatedAt = (new DateTime())->format('Y-m-d H:i:s');
+
         $entityManager = Database::getEntityManager();
-        error_log(json_encode($entityManager->getConnection()->fetchAssociative('SELECT 1 + :number AS sum', ['number' => 3])));
-        return new JsonResponse(['status' => 'O André Moura é um bom dev PHP! 🚀']);
+        $databaseVersion = $entityManager->getConnection()->getServerVersion();
+
+        $databaseMaxConnections = intval($entityManager->getConnection()->fetchOne('SHOW max_connections'));
+
+        $databasename = $_ENV['POSTGRES_DB'];
+        $databaseOpenedConnections = $entityManager->getConnection()->fetchOne("SELECT count(*)::int FROM pg_stat_activity WHERE datname = :databaseName", ['databaseName' => $databasename]);
+
+        return new JsonResponse([
+            'updated_at' => $updatedAt,
+            'dependencies' => [
+                'database' => [
+                    'version' => $databaseVersion,
+                    'max_connections' => $databaseMaxConnections,
+                    'opened_connections' => $databaseOpenedConnections,
+                ],
+            ],
+        ]);
     }
 }
